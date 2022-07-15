fbcc3ac779ec4a24cae2dd2f339b200b18aef10a
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-15 15:23:58 +0530

    Add files via upload

--- database/rraid_db.py
+++ database/rraid_db.py
@@ -1,1 +1,23 @@
+from database import dbb
 
+Rbun = dbb["RBAN"]
+
+
+async def rff(user, reason="#MATHERCHOD"):
+    await Rbun.insert_one({"user": user, "reason": reason})
+
+
+async def runff(user):
+    await Rbun.delete_one({"user": user})
+
+
+async def rban_list():
+    return [lo async for lo in Rbun.find({})]
+
+
+async def ffub_info(user):
+    kk = await Rbun.find_one({"user": user})
+    if not kk:
+        return False
+    else:
+        return kk["reason"]
