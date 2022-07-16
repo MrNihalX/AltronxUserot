e4c183fcb6c386750a125458a4c9ccf95680307a
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-16 19:23:36 +0530

    Add files via upload

--- database/gmute.py
+++ database/gmute.py
@@ -10,6 +10,11 @@ async def gmute(user, reason="#GMuted"):
 async def ungmute(user):
     await gmute.delete_one({"user": user})
 
+
+async def rban_list():
+    return [lo async for lo in gmute.find({})]
+    
+    
 async def is_gmuted(user):
     kk = await gmute.find_one({"user": user})
     if not kk:
