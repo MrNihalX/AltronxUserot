012353bab13504c6db5984b2212467001fd34148
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-07-16 18:15:44 +0530

    Add files via upload

--- database/gmute.py
+++ database/gmute.py
@@ -3,17 +3,17 @@ from config import db
 gmute = db.gmuth
 
 
-async def is_gmuted(sender_id):
-    kk = await gmute.find_one({"sender_id": sender_id})
+async def is_gmuted(user):
+    kk = await gmute.find_one({"user": user})
     if not kk:
         return False
     else:
         return True
 
 
-async def gmute(sender_id, reason="#GMuted"):
-    await gmute.insert_one({"sender_id": sender_id, "reason": reason})
+async def gmute(user, reason="#GMuted"):
+    await gmute.insert_one({"user": user, "reason": reason})
 
 
-async def ungmute(sender_id):
-    await gmute.delete_one({"sender_id": sender_id})
+async def ungmute(user):
+    await gmute.delete_one({"user": user})
