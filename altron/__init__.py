7a8555ace11bef57132ac661b2a73e9e423e57a4
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-08-08 14:27:36 +0530

    Add files via upload

--- altron/__init__.py
+++ altron/__init__.py
@@ -1,21 +1,140 @@
-import glob
-from os.path import dirname, isfile
+import asyncio
+import os
+import time
+from os import listdir, mkdir
+import heroku3
+from aiohttp import ClientSession
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+from motor.motor_asyncio import AsyncIOMotorClient as Bot
+from rich.console import Console
+from rich.table import Table
+from config import MONGO_DB as mango
+from config import OWNER_ID, SESSION1, SESSION2, SESSION3, SESSION4, SESSION5, UPSTREAM_BRANCH, UPSTREAM_REPO
+from altron.modules.client import client1, client2, client3, client4, client5, bot
+from altron.utilities.tasks import install_requirements
+
+loop = asyncio.get_event_loop()
+console = Console()
+
+UPSTREAM_BRANCH = UPSTREAM_BRANCH
+UPSTREAM_REPO = UPSTREAM_REPO
+
+MOD_LOAD = []
+MOD_NOLOAD = []
+
+MONGO_CLI = Bot(mango)
+db = MONGO_CLI.Hero
+
+boottime = time.time()
+
+aiohttpsession = ClientSession()
+
+OWNER_ID = OWNER_ID
+
+bot = bot
+client1 = client1
+client2 = client2
+client3 = client3
+client4 = client4
+client5 = client5
+
+BOT_ID = 0
+BOT_NAME = ""
+BOT_USERNAME = ""
 
 
-def __list_all_modules():
-    work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+async def initiate_bot():
+    global OWNER_ID
+    global BOT_ID, BOT_NAME, BOT_USERNAME
+    global Heroku_cli, Heroku_app
+    os.system("clear")
+    header = Table(show_header=True, header_style="bold yellow")
+    header.add_column(
+        "ALTRON..."
+    )
+    console.print(header)
+    with console.status(
+        "[magenta] Hero Music Bot Booting...",
+    ) as status:
+        console.print("┌ [red]Booting Up The Clients...\n")
+        await bot.start()
+        console.print("└ [green]Booted Bot Client")
+        console.print("\n┌ [red]Booting UserBot Clients...")
+        if SESSION1 != "None":
+            await client1.start()
+            console.print("├ [yellow]Booted Assistant Client")
+        if SESSION2 != "None":
+            await client2.start()
+            console.print("├ [yellow]Booted Assistant Client 2")
+        if SESSION3 != "None":
+            await client3.start()
+            console.print("├ [yellow]Booted Assistant Client 3")
+        if SESSION4 != "None":
+            await client4.start()
+            console.print("├ [yellow]Booted Assistant Client 4")
+        if SESSION5 != "None":
+            await client5.start()
+            console.print("├ [yellow]Booted Assistant Client 5")
+        console.print("└ [green]Assistant Clients Booted Successfully!")
+        if "raw_files" not in listdir():
+            mkdir("raw_files")
+        if "downloads" not in listdir():
+            mkdir("downloads")
+        if "cache" not in listdir():
+            mkdir("cache")
+        if "search" not in listdir():
+            mkdir("search")
+        console.print("\n┌ [red]Loading Clients Information...")
+        getme = await bot.get_me()
+        BOT_ID = getme.id
+        if getme.last_name:
+            BOT_NAME = getme.first_name + " " + getme.last_name
+        else:
+            BOT_NAME = getme.first_name
+        BOT_USERNAME = getme.username
+        console.print("└ [green]Loaded Clients Information!")
+        try:
+            repo = Repo()
+        except GitCommandError:
+            console.print("┌ [red] Checking Git Updates!")
+            console.print("└ [red]Git Command Error\n")
+            return
+        except InvalidGitRepositoryError:
+            console.print("┌ [red] Checking Git Updates!")
+            repo = Repo.init()
+            if "origin" in repo.remotes:
+                origin = repo.remote("origin")
+            else:
+                origin = repo.create_remote("origin", UPSTREAM_REPO)
+            origin.fetch()
+            repo.create_head(UPSTREAM_BRANCH, origin.refs[UPSTREAM_BRANCH])
+            repo.heads[UPSTREAM_BRANCH].set_tracking_branch(
+                origin.refs[UPSTREAM_BRANCH]
+            )
+            repo.heads[UPSTREAM_BRANCH].checkout(True)
+            try:
+                repo.create_remote("origin", UPSTREAM_REPO)
+            except BaseException:
+                pass
+            nrs = repo.remote("origin")
+            nrs.fetch(UPSTREAM_BRANCH)
+            try:
+                nrs.pull(UPSTREAM_BRANCH)
+            except GitCommandError:
+                repo.git.reset("--hard", "FETCH_HEAD")
+            await install_requirements(
+                "pip3 install --no-cache-dir -r requirements.txt"
+            )
+            console.print("└ [red]ɢɪᴛ ᴄʟɪᴇɴᴛ ᴜᴘᴅᴀᴛᴇ ᴄᴏᴍᴘʟᴇᴛᴇᴅ\n")
 
-    all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
-        for f in mod_paths
-        if isfile(f)
-        and f.endswith(".py")
-        and not f.endswith("__init__.py")
-    ]
 
-    return all_modules
+loop.run_until_complete(initiate_bot())
 
 
-ALL_MODULES = sorted(__list_all_modules())
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+def init_db():
+    global db_mem
+    db_mem = {}
+
+
+init_db()
---
+++ altron/__main__.py
@@ -0,0 +1,271 @@
+import asyncio
+import importlib
+import os
+import re
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pytgcalls import idle
+from rich.console import Console
+from rich.table import Table
+from youtubesearchpython import VideosSearch
+
+from config import SESSION1, SESSION2, SESSION3, SESSION4, SESSION5
+from altron import client1, client2, client3, client4, client5, BOT_ID, BOT_NAME, OWNER_ID, bot
+from altron.modules.client import call_py1, call_py2, call_py3, call_py4, call_py5
+from altron.plugins import ALL_MODULES
+from altron.utilities.inline import paginate_modules
+
+loop = asyncio.get_event_loop()
+console = Console()
+HELPABLE = {}
+
+
+async def initiate_bot():
+    with console.status(
+        "[magenta] Finalizing Booting...",
+    ) as status:
+        status.update(
+            status="[bold blue]Scanning for Plugins", spinner="earth"
+        )
+        console.print("Found {} Plugins".format(len(ALL_MODULES)) + "\n")
+        status.update(
+            status="[bold red]Importing Plugins...",
+            spinner="bouncingBall",
+            spinner_style="yellow",
+        )
+        for all_module in ALL_MODULES:
+            imported_module = importlib.import_module(
+                "altron.plugins." + all_module
+            )
+            if (
+                hasattr(imported_module, "__MODULE__")
+                and imported_module.__MODULE__
+            ):
+                imported_module.__MODULE__ = imported_module.__MODULE__
+                if (
+                    hasattr(imported_module, "__HELP__")
+                    and imported_module.__HELP__
+                ):
+                    HELPABLE[
+                        imported_module.__MODULE__.lower()
+                    ] = imported_module
+            console.print(
+                f">> [bold cyan]Successfully imported: [green]{all_module}.py"
+            )
+        console.print("")
+        status.update(
+            status="[bold blue]Importation Completed!",
+        )
+    console.print(
+        "[bold green] ALTRON BOT STARTED \n"
+    )
+    if SESSION1 != "None":
+        try:
+            await client1.join_chat("Modmenumaking")
+            await client1.join_chat("Yaaro_ki_yaarii")
+        except:
+            pass
+        console.print(f"├[red] USERBOT 1 STARTED")
+    if SESSION2 != "None":
+        try:
+            await client2.join_chat("modmenumaking")
+            await client2.join_chat("yaaro_ki_yaarii")
+        except:
+            pass
+        console.print(f"├[red] USERBOT 2 STARTED")
+    if SESSION3 != "None":
+        try:
+            await client3.join_chat("modmenumaking")
+            await client3.join_chat("yaaro_ki_yaarii")
+        except:
+            pass
+        console.print(f"├[red] USERBOT 3 STARTED")
+    if SESSION4 != "None":
+        try:
+            await client4.join_chat("modmenumaking")
+            await client4.join_chat("yaaro_ki_yaarii")
+        except:
+            pass
+        console.print(f"├[red] USERBOT 4 STARTED")
+    if SESSION5 != "None":
+        try:
+            await client5.join_chat("modmenumaking")
+            await client5.join_chat("Yaaro_ki_yaarii")
+        except:
+            pass
+        console.print(f"├[red] USERBOT 5 STARTED")
+    console.print(f"└[red] ALTRON USERBOT BOOT COMPLETED")
+    if SESSION1 != "None":
+        await call_py1.start()
+    if SESSION2 != "None":
+        await call_py2.start()
+    if SESSION3 != "None":
+        await call_py3.start()
+    if SESSION4 != "None":
+        await call_py4.start()
+    if SESSION5 != "None":
+        await call_py5.start()
+    await idle()
+    console.print(f"\n[red] STOPPING BOT")
+
+
+
+
+home_text_pm = f"""**ʜᴇʟʟᴏ ,
+ᴍʏ ɴᴀᴍᴇ ɪs {BOT_NAME}.
+I Aᴍ Gᴇɴɪᴜs, Aɴ Aᴅᴠᴀɴᴄᴇᴅ UsᴇʀBᴏᴛ Wɪᴛʜ Sᴏᴍᴇ Usᴇғᴜʟ Fᴇᴀᴛᴜʀᴇs.**"""
+
+
+@bot.on_message(command(["start"]) & filters.private)
+async def start(_, message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/027283ee9defebc3298b8.png",
+        caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
+💥 Hᴇʟʟᴏ, I Aᴍ Gᴇɴɪᴜs » Aɴ Aᴅᴠᴀɴᴄᴇᴅ
+Pʀᴇᴍɪᴜᴍ Tᴇʟᴇɢʀᴀᴍ Usᴇʀ Bᴏᴛ.
+
+┏━━━━━━━━━━━━━━━━━━━┓
+┣★ Oᴡɴᴇʀ'xD› : [Aᴅɪᴛʏᴀ Hᴀʟᴅᴇʀ](https://t.me/adityahalder)
+┣★ Uᴘᴅᴀᴛᴇs ›› : [Aᴅɪᴛʏᴀ Sᴇʀᴠᴇʀ](https://t.me/adityaserver)
+┣★ Sᴜᴘᴘᴏʀᴛ » : [Aᴅɪᴛʏᴀ Dɪsᴄᴜs](https://t.me/adityadiscus)
+┗━━━━━━━━━━━━━━━━━━━┛
+
+💞 Cʟɪᴄᴋ Oɴ Dᴇᴘʟᴏʏ Bᴜᴛᴛᴏɴ Tᴏ Mᴀᴋᴇ
+Yᴏᴜʀ Oᴡɴ » Gᴇɴɪᴜs Usᴇʀ Bᴏᴛ.
+━━━━━━━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 Dᴇᴘʟᴏʏ Gᴇɴɪᴜs UsᴇʀBᴏᴛ ✨", url=f"https://github.com/GeniusBoi/Genius-UserBot")
+                ]
+                
+           ]
+        ),
+    )
+    
+    
+    
+@bot.on_message(command(["help"]) & SUDOERS)
+async def help_command(_, message):
+    text, keyboard = await help_parser(message.from_user.mention)
+    await bot.send_message(text, reply_markup=keyboard)
+
+
+
+
+async def help_parser(name, keyboard=None):
+    if not keyboard:
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+    return (
+        """**🥀 Wᴇʟᴄᴏᴍᴇ Tᴏ Hᴇʟᴘ Mᴇɴᴜ Oғ :
+Gᴇɴɪᴜs UsᴇʀBᴏᴛ Vᴇʀ : `2.0` 🔥...
+
+💞 Jᴜsᴛ Cʟɪᴄᴋ Oɴ Bᴇʟᴏᴡ Iɴʟɪɴᴇ
+Tᴏ Gᴇᴛ Gᴇɴɪᴜs Cᴏᴍᴍᴀɴᴅs ✨...**
+""".format(
+            first_name=name
+        ),
+        keyboard,
+    )
+
+@bot.on_callback_query(filters.regex("close") & SUDOERS)
+async def close(_, CallbackQuery):
+    await CallbackQuery.message.delete()
+
+@bot.on_callback_query(filters.regex("hero") & SUDOERS)
+async def hero(_, CallbackQuery):
+    text, keyboard = await help_parser(CallbackQuery.from_user.mention)
+    await CallbackQuery.message.edit(text, reply_markup=keyboard)
+
+
+@bot.on_callback_query(filters.regex(r"help_(.*?)") & SUDOERS)
+async def help_button(client, query):
+    home_match = re.match(r"help_home\((.+?)\)", query.data)
+    mod_match = re.match(r"help_module\((.+?)\)", query.data)
+    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
+    next_match = re.match(r"help_next\((.+?)\)", query.data)
+    back_match = re.match(r"help_back", query.data)
+    create_match = re.match(r"help_create", query.data)
+    top_text = f"""**🥀 Wᴇʟᴄᴏᴍᴇ Tᴏ Hᴇʟᴘ Mᴇɴᴜ Oғ :
+Gᴇɴɪᴜs UsᴇʀBᴏᴛ Vᴇʀ : `2.0` 🔥...
+
+💞 Jᴜsᴛ Cʟɪᴄᴋ Oɴ Bᴇʟᴏᴡ Iɴʟɪɴᴇ
+Tᴏ Gᴇᴛ Gᴇɴɪᴜs Cᴏᴍᴍᴀɴᴅs ✨...**
+ """
+    if mod_match:
+        module = mod_match.group(1)
+        text = (
+            "{} **{}**:\n".format(
+                "**🥀 Wᴇʟᴄᴏᴍᴇ Tᴏ Hᴇʟᴘ Mᴇɴᴜ Oғ :** ", HELPABLE[module].__MODULE__
+            )
+            + HELPABLE[module].__HELP__
+        )
+        key = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="↪️ ʙᴀᴄᴋ", callback_data="help_back"
+                    ),
+                    InlineKeyboardButton(
+                        text="🔄 ᴄʟᴏsᴇ", callback_data="close"
+                    ),
+                ],
+            ]
+        )
+
+        await query.message.edit(
+            text=text,
+            reply_markup=key,
+            disable_web_page_preview=True,
+        )
+    elif home_match:
+        out = private_panel()
+        await bot.send_message(
+            query.from_user.id,
+            text=home_text_pm,
+            reply_markup=InlineKeyboardMarkup(out[1]),
+        )
+        await query.message.delete()
+    elif prev_match:
+        curr_page = int(prev_match.group(1))
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(curr_page - 1, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif next_match:
+        next_page = int(next_match.group(1))
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(next_page + 1, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif back_match:
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(0, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif create_match:
+        text, keyboard = await help_parser(query)
+        await query.message.edit(
+            text=text,
+            reply_markup=keyboard,
+            disable_web_page_preview=True,
+        )
+
+    return await client.answer_callback_query(query.id)
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(initiate_bot())
