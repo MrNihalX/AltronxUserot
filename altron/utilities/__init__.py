def2f04142a5249bd43df0e436cadd5b8c3450dd
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-08-09 07:55:59 +0530

    Update __init__.py

--- altron/utilities/__init__.py
+++ altron/utilities/__init__.py
@@ -1,14 +1,13 @@
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from altron.modules.database import cli
 from pymongo import MongoClient
 from pyrogram import Client
 from config import *
 from ..logger import LOGGER
 
 
-_mongo_async_ = _mongo_client_(MONGO_DB)
 _mongo_sync_ = MongoClient(MONGO_DB)
 
-mongodb = _mongo_async_.altron
+mongodb = cli.altron
 pymongodb = _mongo_sync_.altron
 
-dbb = _mongo_async_["ALTRONDB"]
+dbb = cli["ALTRONDB"]
