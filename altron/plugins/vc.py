4114306e6836b2f49f58b48048bdac06c8dbabe8
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-08-10 11:52:00 +0530

    Update vc.py

--- altron/plugins/vc.py
+++ altron/plugins/vc.py
@@ -7,7 +7,7 @@ from pyrogram import *
 @client1.on_message(filters.command(["startvc"]))
 async def _(c:client1, m:Message):
     try:
-        await c.CreateGroupCall(m.chat_id)
+        await c(CreateGroupCall(m.chat_id)) 
         await m.reply_text("**🔊 Voice Chat Started Successfully**")
     except:
         pass
